9c4ceb38e56e33874e85ef219863644eeacf982f
Nekarone <73489700+Nekarone@users.noreply.github.com>
2022-02-24 21:12:01 +0000

    Added answer writer
    Needs the amount of projects planned, the project names, and a hashmap of the projects to the worker names

--- Runner.java
+++ Runner.java
@@ -1,7 +1,27 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Runner {
     
     public static void main(String args[]) {
 
     }
 
+    public static void createPlainTxt(int planned, String[] projectNames, HashMap<String, String> answers){
+        try {
+            FileWriter writer = new FileWriter("Hashcode_2022_answer");
+            writer.write(planned + "\n");
+            for(int i = 0; i < planned*2; i++){
+                String project = projectNames[i];
+                writer.write(project + "\n");
+                writer.write(answers.get(project) + "\n");
+            }
+            writer.close();
+        } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
+        }
+    }
 }
