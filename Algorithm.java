aca663614169af9ae707aa354e6ce7c6ab10550b
ishdafish24 <51826348+ishdafish24@users.noreply.github.com>
2022-02-24 20:42:07 +0000

    Algo

---
+++ Algorithm.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Algorithm {
+    
+    private static ArrayList<String> worker = new ArrayList<String>();
+        public static void main(String[] args) {
+            Database data = new Database("b_better_start_small.im");
+            
+
+    }
+}
