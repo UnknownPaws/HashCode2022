709568df5b34a799f558c1f7a48c8ae2fb5aed5a
ishdafish24 <51826348+ishdafish24@users.noreply.github.com>
2022-02-24 20:58:16 +0000

    Progress. Nah

--- Algorithm.java
+++ Algorithm.java
@@ -2,15 +2,26 @@ import java.util.*;
 
 public class Algorithm {
 
+    private Database data;
+    private ArrayList<String> allskills = new ArrayList<String>();
 
+    public Algorithm(String filename) {
+        data = new Database(filename);
 
-    public Algorithm() {
-
+    }
+    public void Sort() {
+        ArrayList<Project> p = data.getProjects();
+        ArrayList<Worker> w = data.getWorkers();
+        int s = w.size();
+        for (int i = 0; i < s; i++){
+            int hashSize = w.get(i).getSkillToLevel().size();
+            for (int j = 0; j < hashSize; j++) {
+                // allskills.add(w.get(i).getSkillToLevel().get;
+            }
+        }
+       
     }
     
-    private static ArrayList<String> worker = new ArrayList<String>();
         
-        
-
-    }
+}
 
