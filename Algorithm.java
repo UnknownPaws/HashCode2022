72b4da1d32c0fbaa47ede1d1d26f4327d3db764a
ishdafish24 <51826348+ishdafish24@users.noreply.github.com>
2022-02-24 21:18:01 +0000

    Algo

--- Algorithm.java
+++ Algorithm.java
@@ -3,22 +3,36 @@ import java.util.*;
 public class Algorithm {
 
     private Database data;
-    private ArrayList<String> allskills = new ArrayList<String>();
+    private ArrayList<String> allWorkerSkills = new ArrayList<String>();
+    private ArrayList<String> allProjectSkills = new ArrayList<String>();
 
     public Algorithm(String filename) {
         data = new Database(filename);
 
     }
+    // 
     public void Sort() {
         ArrayList<Project> p = data.getProjects();
         ArrayList<Worker> w = data.getWorkers();
         int s = w.size();
+        int d = p.size();
         for (int i = 0; i < s; i++){
             int hashSize = w.get(i).getSkillToLevel().size();
             for (int j = 0; j < hashSize; j++) {
-                // allskills.add(w.get(i).getSkillToLevel().get;
+                allWorkerSkills.add(w.get(j).getSkills().get(j));
             }
         }
+        Set set1 = new HashSet(allWorkerSkills);
+        ArrayList
+        for (int i = 0; i < d; i++){
+            int hashSize = p.get(i).getRoles().size();
+            for (int j = 0; j < hashSize; j++) {
+                allProjectSkills.add(p.get(j).getRoles().get(j));
+            }
+        }
+        Set set2 = new HashSet<>(allProjectSkills);
+        
+
        
     }
     
