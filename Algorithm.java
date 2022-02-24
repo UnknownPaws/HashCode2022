0fef8037208a6df961ce6972d20f6399ba4c0f12
ishdafish24 <51826348+ishdafish24@users.noreply.github.com>
2022-02-24 20:45:12 +0000

    Forgot Runner

--- Algorithm.java
+++ Algorithm.java
@@ -1,11 +1,16 @@
 import java.util.*;
 
 public class Algorithm {
-    
-    private static ArrayList<String> worker = new ArrayList<String>();
-        public static void main(String[] args) {
-            Database data = new Database("b_better_start_small.im");
-            
+
+
+
+    public Algorithm() {
 
     }
-}
+    
+    private static ArrayList<String> worker = new ArrayList<String>();
+        
+        
+
+    }
+
